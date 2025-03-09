bd84e30fd95115a34d4029643d7349dcfb91bca1
CoolShivas <shivas2710cool00@gmail.com>
2025-03-09 15:37:29 +0530

    Creating and Organizing Module

---
+++ app.js
@@ -0,0 +1,7 @@
+const addFunction = require("./math");
+
+console.log(addFunction(5 , 2));
+// // Open the Terminal, and write the command node app.js
+// // Here, we are getting the output on TERMINAL as :-
+// // 7
+
